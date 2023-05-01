f948ca7f3576616c6b965654e7938bad8b1bb6d8
Christline Edward <cedward@fandm.edu>
2023-05-01 16:03:51 -0400

    complete function

--- urlCompress.py
+++ urlCompress.py
@@ -7,9 +7,13 @@ import pyshorteners
 
 def shortenUrl(longUrl):
     '''takes a long URL as a parameter and returns a shortened URL'''
-   
+    #  create an instance of the Shortener class from the pyshorteners module
     s = pyshorteners.Shortener()
 
+    # # use the tinyurl service to shorten the long URL and return the result
     return s.tinyurl.short(longUrl)
     
     
+if __name__ == '__main__':
+    print(shortenUrl("https://fandm.instructure.com/?login_success=1"))
+   
