e249abc01a8609e27544fd737cb44a6ef7aa0a96
Christline Edward <cedward@fandm.edu>
2023-05-24 13:16:28 -0400

    type hint edits

--- urlCompress.py
+++ urlCompress.py
@@ -1,36 +1,37 @@
 # NOTES 
 # 1. To generate a shortened URL, you can import from python library:
 #   - pyshorteners
-#   - shortuuid
-
-import shortuuid
+#   - shortuuid *** (universally unique identifier)
+from typing import Dict, Optional
+import shortuuid 
 
 class URLShortener:
     # initialize 'url_map' attr to an empty dict
-    def __int__(self):
+    def __init__(self):
         '''self = is just conventional naming for 1st parameter, url_map = attribute/instance variable that will store the mappings between short codes and long URLs '''
         # initializing to empty dict - ensures each instance of the 'URLShortener' class 
         # will have its own independent 'url_map' to store the mappings
-        self.url_map = {}
+        self.url_map = Dict[str, str] = {}
+        print(self.url_map)
         
-    def add_url_mapping(self, long_url):
+    def add_mapping(self, long_url: str) -> str:
         '''takes a long URL as a parameter and returns a shortened URL'''
-    #  create an instance of the Shortener class from the pyshorteners module
-    s = pyshorteners.Shortener()
+        # Generate a short code using the shortuuid module
+        short_code: str = shortuuid.uuid()[:8]
 
-    # # use the tinyurl service to shorten the long URL and return the result
-    return s.tinyurl.short(longUrl)
-    
+        # Add the mapping of short code to longURL in the url_map dict
+        self.url_map[short_code] = long_url
+
         # Return the generated short code
         return short_code
     
-    def remove_mapping(self, short_code):
+    def remove_mapping(self, short_code: str) -> None:
         # Check if short code exists in the url_map dict
         if short_code in self.url_map:
             # Remoce the mapping from the url_map dict
             del self.url_map[short_code]
 
-    def get_long_url(self, short_code):
+    def get_long_url(self, short_code: str) -> Optional[str]:
         # Retrieve long URL associated w the given short code from the url_map dict
         return self.url_map.get(short_code)
     
@@ -66,5 +67,8 @@ if __name__ == '__main__':
     # this prints out the contents of the current mappings
     print("\nurl_map dictionary: ")
     print(shortener.url_map)
+
+
+
     # print(short_code("https://fandm.instructure.com/?login_success=1"))
    
