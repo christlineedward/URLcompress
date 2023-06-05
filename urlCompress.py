96aa4a07e0cd3404c4b4e72661337b9770a1b91a
Christline Edward <cedward@fandm.edu>
2023-06-05 19:11:25 -0400

    removing main code to separate file

--- urlCompress.py
+++ urlCompress.py
@@ -34,7 +34,7 @@ class URLShortener:
     
     def remove_mapping(self, short_code: str) -> None:
         # find the URLMapping object with the given short code and delete it from the database
-        URLMapping.onjects.filter(short_code=short_code).delete()
+        URLMapping.objects.filter(short_code=short_code).delete()
 
     def get_long_url(self, short_code: str) -> Optional[str]:
         try:
@@ -49,35 +49,4 @@ class URLShortener:
             return None
 
     
-if __name__ == '__main__':
 
-    # TESTS
-    # create an instance of URLShortener
-    shortener = URLShortener()
-
-    # add new mappings
-    shortcode1 = shortener.add_mapping("https://www.notion.so/tristanwedderburn/May-1st-Sync-Meeting-Notes-d849692fbcfd4bcbaecfc4cb734de894")
-    shortcode2 = shortener.add_mapping("https://github.com/christlineedward/URLcompress")
-
-    # print the short codes and their corresponding long URLs 
-    print("Short codes and long URLs: ")
-    print(f"{shortcode1}: {shortener.get_long_url(shortcode1)}")
-    print(f"{shortcode2}: {shortener.get_long_url(shortcode2)}")
-
-    # remove a mapping
-    shortener.remove_mapping(shortcode1)
-
-    # print the updated mappings
-    print("\nAfter removing a mapping: ")
-    print(f"{shortcode1}: {shortener.get_long_url(shortcode1)}")
-    print(f"{shortcode2}: {shortener.get_long_url(shortcode2)}")
-
-    # allow user input
-    long_url = input("\nEnter a long URL to shorten: ")
-    short_code = shortener.add_mapping(long_url)
-    print(f"\nShortened URL: {short_code}")
-    print(f"Original URL: {shortener.get_long_url(short_code)}")
-    
-    # this prints out the contents of the current mappings
-    print("\nurl_map dictionary: ")
-    print(shortener.url_map)
