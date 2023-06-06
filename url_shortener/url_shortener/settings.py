2602bd6c80517332ccc08a37c833c1b5d86d5365
Christline Edward <cedward@fandm.edu>
2023-06-05 22:27:01 -0400

    modifying settings.py in app

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "url_shortener_app",
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,15 @@ WSGI_APPLICATION = "url_shortener.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db",
+        "USER": "user",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": 5432,
     }
 }
+
 
 
 # Password validation
